58fd578c21ca2575d6d6fbe58701632c48c9c673
Viktor Gavrilov <Gavr320@yandex.ru>
2025-02-05 01:08:14 +0300

    Обновлены тесты и логика истории задач

--- src/test/manager/InMemoryHistoryManagerTest.java
+++ src/test/manager/InMemoryHistoryManagerTest.java
@@ -8,6 +8,7 @@ import org.junit.jupiter.api.Test;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
+
 public class InMemoryHistoryManagerTest {
 
     private HistoryManager historyManager;

0d916216328c751e58c0a8463d304efbefefbb71
Viktor Gavrilov <Gavr320@yandex.ru>
2025-03-03 23:51:06 +0300

    Реализована новая функциональность, добавлены и обновлены тесты.

--- src/main/java/manager/InMemoryTaskManager.java
+++ src/main/java/manager/InMemoryTaskManager.java
@@ -9,7 +9,9 @@ import java.util.*;
 
 public class InMemoryTaskManager implements TaskManager {
     protected TreeSet<Task> prioritizedTasks = new TreeSet<>(new TaskComparator());
+
     static class TaskComparator implements Comparator<Task> {
+
         @Override
         public int compare(Task task1, Task task2) {
             if (task1.getStartTime().equals(task2.getStartTime())) {
@@ -18,6 +20,7 @@ public class InMemoryTaskManager implements TaskManager {
             return task1.getStartTime().compareTo(task2.getStartTime());
         }
     }
+
     protected final Map<Integer, Task> tasks = new HashMap<>();
     protected final Map<Integer, Epic> epics = new HashMap<>();
     protected final Map<Integer, Subtask> subtasks = new HashMap<>();
@@ -121,7 +124,8 @@ public class InMemoryTaskManager implements TaskManager {
             epic.getSubtasksId().forEach(subId -> {
                 prioritizedTasks.remove(subtasks.get(subId));
                 subtasks.remove(subId);
-                historyManager.remove(subId);});
+                historyManager.remove(subId);
+            });
         }
     }
 
@@ -173,7 +177,8 @@ public class InMemoryTaskManager implements TaskManager {
     @Override
     public void updateTask(Subtask subtask) {
         if (subtasks.containsKey(subtask.getId())) {
-            if (intersectionTimeTask(subtask)) throw new IllegalArgumentException("Задача пересекается с другой задачей");
+            if (intersectionTimeTask(subtask))
+                throw new IllegalArgumentException("Задача пересекается с другой задачей");
             subtasks.put(subtask.getId(), subtask);
             prioritizedTasks.remove(subtask);
             addToPrioritizedTasks(subtask);
